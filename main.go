da1f52e421da6f92ea6cb00ddf2c48af8c0f484d
Peh <peh@example.com>
2020-08-03 21:55:54 +0100

    Thinkering about how to structure the API. Changes to main.go

--- main.go
+++ main.go
@@ -6,8 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 ) 
 
+func handleOCRConversion(c *gin.Context){
+	//To be implemented
+}
+
 func main() {
 	r := gin.Default()
+	
+	r.POST("/ocr-conversion", handleOCRConversion)
+	//TODO no need to handle static pages as they would be served by React
+	//We'll be only building an api back-end for the moment.
+	//r.GET("/", handleHomepage???)
+	
 	r.Run()
+
 }
 
